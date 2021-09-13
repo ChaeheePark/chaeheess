6830e7d5f22a94acd9320b98a3b591a9f9afbc96
ChaeheePark <chaeheepark99@gmail.com>
2021-09-13 11:41:28 +0900

    bigdata_week2

---
+++ COURSES for my major/21-2_Bigdata_Analysis/src/mylib.py
@@ -0,0 +1,11 @@
+def sayHello():
+    print("hello")
+
+def getKey(keyPath):
+    d=dict()
+    f=open(keyPath,'r')
+    for line in f.readlines():
+        row=line.split('=')
+        row0=row[0]
+        d[row0]=row[1].strip()
+    return d
