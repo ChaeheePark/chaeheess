6b57af8b277e8360a26b2ab468cdfccf175cc101
ChaeheePark <chaeheepark99@gmail.com>
2021-12-23 20:36:03 +0900

    blockchain

---
+++ COURSES for my major/21-1_BlockChain_Programming/myProject/_gethNow.bat
@@ -0,0 +1,1 @@
+geth --identity "chaeh" --allow-insecure-unlock --datadir .\eth --rpc --rpcaddr "localhost" --rpcport "8445" --rpccorsdomain "*" --port "38445" --rpcapi "admin, db, eth, debug, miner, net, shh, txpool, personal, web3" --networkid 33
